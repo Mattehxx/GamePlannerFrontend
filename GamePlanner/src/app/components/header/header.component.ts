001a218049a2e846dd6974cec0f61a10aac9b527
Luca Gallazzi <luca.gallazzi@itsincom.it>
2024-11-12 10:54:30 +0100

    add modal toggle to Header

--- GamePlanner/src/app/components/header/header.component.ts
+++ GamePlanner/src/app/components/header/header.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
-
+export class HeaderComponent implements AfterViewInit {
+  
+  
+  @ViewChild('modal') modalElement!: ElementRef;
+  @ViewChild('overlay') overlayElement!: ElementRef;
+  
+  isLogged: boolean = true;
+  isModalOpen: boolean = false;
+  user : User = 
+  {
+    UserId: "1",
+    Name: "Alice",
+    Surname: "Smith",
+    Email: "alice.smith@example.com",
+    Phone: "123-456-7890",
+    BirthDate: new Date("1990-05-15"),
+    ImgUrl: undefined, // Puoi sostituirlo con un'immagine fittizia
+    CanBeMaster: true,
+    KnowledgeId: 1,
+    Knowledge: { KnowledgeId: 1, Name: "D&D" }
+  }
+  
+  ngAfterViewInit(): void {
+    
+  }
+  
+  
+  toggleModal() {
+    this.isModalOpen = !this.isModalOpen
+  }
+  
+  closeModal() {
+    this.isModalOpen = false;
+  }
+  
+    @HostListener('document:click', ['$event'])
+    handleClickOutside(event: MouseEvent) {
+      // Verifica se il clic è fuori dalla modale o dall'overlay
+      if (this.isModalOpen && !this.modalElement.nativeElement.contains(event.target) && !this.overlayElement.nativeElement.contains(event.target)) {
+        this.closeModal();
+      }
+    }
 }
---
+++ GamePlanner/src/app/models/knowledge.model.ts
@@ -0,0 +1,4 @@
+export interface Knowledge{
+    KnowledgeId : number;
+    Name: string;
+}
