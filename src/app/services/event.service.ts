7be9e61a5b263ee315bcf5fdb82e8e4c6647e70f
Leonardo Galante <leonardo.galante@itsincom.it>
2024-11-18 12:32:32 +0100

    Add event service and patchJsonModel

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 import { EventModel } from '../models/event.model';
+import { BehaviorSubject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { environment } from '../environment/environment';
+import { ODataResponse } from '../models/odataResponse.model';
+import { EventInputModel } from '../models/input-models/event.input.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +13,51 @@ import { EventModel } from '../models/event.model';
 export class EventService {
 
   eventDetail: EventModel | undefined;
+  deletedEventDetail : EventModel | undefined;
+  odataQuery: string | undefined;
+
+  private eventSubject = new BehaviorSubject<EventModel[]>([]);
+  isModalOpen: boolean = false;
+  event$ = this.eventSubject.asObservable();
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  //#region CRUD
+  public get() {
+    this.http.get<ODataResponse<EventModel>>(`${environment.apiUrl}odata/Event
+      ${this.odataQuery && this.odataQuery.trim() != ""
+        ? `/${this.odataQuery}` : ""}`).subscribe({
+          next: (res) => {
+            this.eventSubject.next(res.value);
+          }, error: (msg) => {
+            console.error(msg);
+          }
+        });
+  }
+  public post(model : EventInputModel){
+    this.http.post<EventModel>(`${environment.apiUrl}api/Event`,model).subscribe({
+      next: (res) => {
+        const events = this.eventSubject.value;
+        events.push(res);
+        this.eventSubject.next(events);
+      },error : (msg) => {
+        console.error(msg);
+      }
+    });
+  }
+  public delete(id : number){
+    this.http.delete<EventModel>(`${environment.apiUrl}/api/Event/${id}`).subscribe({
+      next: (res) => {
+        this.deletedEventDetail = res;
+      },error: (msg)=>{
+        console.error(msg);
+      }
+    });
+  }
+  public patch(){
+    
+  }
+  
+  //#endregion
 
 }
