67edb28223bca99bd864981374cb466e21fb9b35
GabryTm047 <gabriele.costa@itsincom.it>
2024-11-19 12:15:58 +0100

    refactor styles and implement KnowledgeService for better user experience

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,8 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { GameModel } from '../../models/game.model';
+import { knowledgeModel } from '../../models/knowledge.model';
+import { preferenceModel } from '../../models/preference.model';
 import { User } from '../../models/user.model';
 import { AuthService } from '../../services/auth.service';
+import { GameService } from '../../services/game.service';
+import { KnowledgeService } from '../../services/knowledge.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -12,20 +17,33 @@ import { AuthService } from '../../services/auth.service';
   styleUrl: './user-profile.component.scss'
 })
 export class UserProfileComponent implements OnInit {
+
+  PostPreference() {
+    throw new Error('Method not implemented.');
+    //Post di reference
+  }
+
+  selectedImageFile: File | undefined;
+  selectedImagePreview: string | undefined;
+  games: GameModel[] = [];
+  knowledges: knowledgeModel[] = [];
   userToEdit: User | null = null;
   originalUser: User | null = null;
   userForm: FormGroup;
+  viewMode: string = 'default';
 
-  constructor(public as: AuthService, private fb: FormBuilder) {
+  constructor(public as: AuthService, private fb: FormBuilder, public gs: GameService, public ks: KnowledgeService) {
+    this.games = this.gs.Games$.value;
+    console.log('Games:', this.games);
     this.userForm = this.fb.group({
       email: [null, [Validators.required, Validators.email]],
       name: [null, Validators.required],
       surname: [null, Validators.required],
       phone: [null],
-      birthdate: [null]
+      birthdate: [null],
+      preferences: this.fb.array([])
     });
   }
-
   ngOnInit(): void {
     if (this.as.isLogged) {
       this.as.user?.subscribe({
@@ -38,6 +56,7 @@ export class UserProfileComponent implements OnInit {
             this.userForm.controls['surname'].setValue(user.surname);
             this.userForm.controls['phone'].setValue(user.phoneNumber ?? null);
             this.userForm.controls['birthdate'].setValue(user.birthDate ? new Date(user.birthDate).toISOString().split('T')[0] : null);
+            this.setPreferences(user.preferences ?? []);
           } else {
             console.error('User is undefined');
           }
@@ -47,7 +66,33 @@ export class UserProfileComponent implements OnInit {
         }
       });
     }
+    this.gs.Games$.subscribe({
+      next: (games) => {
+        this.games = games;
+      }
+    })
+    this.ks.Knowledges$.subscribe({
+      next: (knowledges) => {
+        this.knowledges = knowledges;
+      },
+    })
   }
+  changeViewMode(viewMode: string) {
+    this.viewMode = viewMode;
+  }
+  setPreferences(preferences: preferenceModel[]): void {
+    const preferenceFGs = preferences.map(pref => this.fb.group(pref));
+    const preferenceFormArray = this.fb.array(preferenceFGs);
+    this.userForm.setControl('preferences', preferenceFormArray);
+  }
+
+  // addPreference(): void {
+  //   this.preferences.push(this.fb.control('', Validators.required));
+  // }
+
+  // removePreference(index: number): void {
+  //   this.preferences.removeAt(index);
+  // }
 
   removeImg() {
     if (this.userToEdit) {
@@ -64,6 +109,7 @@ export class UserProfileComponent implements OnInit {
       this.userForm.controls['phone'].setValue(this.originalUser.phoneNumber ?? null);
       this.userForm.controls['birthdate'].setValue(this.originalUser.birthDate ? new Date(this.originalUser.birthDate).toISOString().split('T')[0] : null);
     }
+    this.viewMode = 'default';
   }
 
   onSubmit(): void {
@@ -72,5 +118,33 @@ export class UserProfileComponent implements OnInit {
       // Esegui la patch con updatedUser
       console.log('Updated user:', updatedUser);
     }
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+
+      if (!file.type.startsWith('image/')) {
+        console.error('The selected file is not an image.');
+        return;
+      }
+      this.selectedImageFile = file;
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.selectedImagePreview = reader.result as string;
+      };
+
+      reader.onerror = () => {
+        console.error('Error reading file.');
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
+  get preferences(): FormArray {
+    return this.userForm.get('preferences') as FormArray;
   }
 }
